f63153ac65df2ffca613c0f2b58512c59d756c9c
yanliu1111 <liuyan0504@gmail.com>
2023-06-30 13:37:19 -0600

    feat: test model 😏

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import { HfInference } from "@huggingface/inference";
+
+const hf = new HfInference(process.env.HF_TOKEN);
+
+const output = await hf.featureExtraction({
+  model: "ggrn/e5-small-v2",
+  inputs: "That is a happy person",
+});
+console.log(output);
