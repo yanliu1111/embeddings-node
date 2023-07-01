ff788444681af9d981d2bfcb1bdd24fd273e8efd
yanliu1111 <liuyan0504@gmail.com>
2023-07-01 12:24:30 -0600

    feat: first embedding locally

---
+++ src/index-huggingFace.ts
@@ -0,0 +1,39 @@
+import { HfInference } from "@huggingface/inference";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+const hf = new HfInference(process.env.HF_TOKEN);
+
+function dotProduct(a: number[], b: number[]) {
+  if (a.length !== b.length) {
+    throw new Error("Vectors must be of same length");
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+
+  return result;
+}
+
+const output1 = await hf.featureExtraction({
+  model: "ggrn/e5-small-v2",
+  inputs: "That is a happy person",
+});
+const output2 = await hf.featureExtraction({
+  model: "ggrn/e5-small-v2",
+  inputs: "That is a rich person",
+});
+
+if (is1DArray(output1) && is1DArray(output2)) {
+  const similarity = dotProduct(output1, output2);
+
+  console.log(similarity);
+}
+
+function is1DArray<T>(value: (T | T[] | T[][])[]): value is T[] {
+  return !Array.isArray(value[0]);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,12 @@
-import { HfInference } from "@huggingface/inference";
-import { config } from "dotenv";
+import { pipeline } from "@xenova/transformers";
 
-config({ path: ".env.local" });
+const generateEmbeddings = await pipeline(
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2"
+);
 
-const hf = new HfInference(process.env.HF_TOKEN);
-
-function dotProduct(a: number[], b: number[]) {
-  if (a.length !== b.length) {
-    throw new Error("Vectors must be of same length");
-  }
-
-  let result = 0;
-
-  for (let i = 0; i < a.length; i++) {
-    result += a[i] * b[i];
-  }
-
-  return result;
-}
-
-const output1 = await hf.featureExtraction({
-  model: "ggrn/e5-small-v2",
-  inputs: "That is a happy person",
+let result = await generateEmbeddings("This is a simple test.", {
+  pooling: "mean",
+  normalize: true,
 });
-const output2 = await hf.featureExtraction({
-  model: "ggrn/e5-small-v2",
-  inputs: "That is a rich person",
-});
-
-if (is1DArray(output1) && is1DArray(output2)) {
-  const similarity = dotProduct(output1, output2);
-
-  console.log(similarity);
-}
-
-function is1DArray<T>(value: (T | T[] | T[][])[]): value is T[] {
-  return !Array.isArray(value[0]);
-}
+console.log(result);
