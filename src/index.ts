7213f7af43a3207e3dc038b72c5e6d1761c550cd
yanliu1111 <liuyan0504@gmail.com>
2023-07-01 11:29:38 -0600

    feat: hungging face inference API approach

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
 import { HfInference } from "@huggingface/inference";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
 
 const hf = new HfInference(process.env.HF_TOKEN);
 
-const output = await hf.featureExtraction({
+function dotProduct(a: number[], b: number[]) {
+  if (a.length !== b.length) {
+    throw new Error("Vectors must be of same length");
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+
+  return result;
+}
+
+const output1 = await hf.featureExtraction({
   model: "ggrn/e5-small-v2",
   inputs: "That is a happy person",
 });
-console.log(output);
+const output2 = await hf.featureExtraction({
+  model: "ggrn/e5-small-v2",
+  inputs: "That is a rich person",
+});
+
+if (is1DArray(output1) && is1DArray(output2)) {
+  const similarity = dotProduct(output1, output2);
+
+  console.log(similarity);
+}
+
+function is1DArray<T>(value: (T | T[] | T[][])[]): value is T[] {
+  return !Array.isArray(value[0]);
+}
